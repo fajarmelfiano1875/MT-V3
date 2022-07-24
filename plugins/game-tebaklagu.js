bea784a81046c07efa1b3e5a97b26ad1358c2d88
FajarMelfiano <melfin795@gmail.com>
2022-07-24 22:38:09 +0700

    fix

--- plugins/game-tebaklagu.js
+++ plugins/game-tebaklagu.js
@@ -29,7 +29,7 @@ Bonus: ${poin} XP
             delete conn.tebaklagu[id]
         }, timeout)
     ]
-    await conn.sendFile(m.chat, json.preview, 'coba-lagi.mp3', '', m)
+    await conn.sendFile(m.chat, json.preview , 'coba-lagi.mp3', '', m)
 }
 handler.help = ['tebaklagu']
 handler.tags = ['game']
