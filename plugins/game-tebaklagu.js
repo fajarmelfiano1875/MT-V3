b5873128ad36c48a89f8e93a75bf401ce5561bef
FajarMelfiano <melfin795@gmail.com>
2022-07-24 14:46:48 +0700

    tebaklagu

--- plugins/game-tebaklagu.js
+++ plugins/game-tebaklagu.js
@@ -10,7 +10,7 @@ let handler = async (m, { conn, usedPrefix }) => {
         throw false
     }
     // ubah isi 'id' kalo mau ganti playlist spotifynya
-    let res = await fetch(global.API('xteam', '/game/tebaklirik/', { id: '5FQ6ndnAIfjIwZmKoC8lGf' }, 'APIKEY'))
+    let res = await fetch(global.API('xteam', '/game/tebaklagu/', { id: '3AaKHE9ZMMEdyRadsg8rcy' }, 'APIKEY'))
     if (res.status !== 200) throw await res.text()
     let result = await res.json()
     let json = result.result
