cfacbaafbbb571721485b49e7992e4a564d713df
FajarMelfiano <melfin795@gmail.com>
2022-07-24 13:49:19 +0700

    fix

--- plugins/internet-masak.js
+++ plugins/internet-masak.js
@@ -3,7 +3,7 @@ let fetch = require("node-fetch")
 let handler = async (m, { conn, text }) => {
   let more = String.fromCharCode(8206)
   let readMore = more.repeat(4001)
-  let res = await fetch(global.API('zeks', '/api/resep-masak', { q : text }, 'apikey'))
+  let res = await fetch(global.API('zekais', '/api/resep', { q : text }, 'apikey'))
   if (!res.ok) throw await res.text()
   let json = await res.json()
   if (!json.title) throw json
